0fe4aae4ee5850cc31f8ea8c4f082eb7874efb74
Adil Valiyev <avliyev17@gmail.com>
2025-11-23 07:49:24 +0000

    sdfghjk

---
+++ python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Square class that can print itself using #."""
+
+
+class Square:
+    """A class that defines a square."""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieves the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size with validation."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints the square using #."""
+        if self.__size == 0:
+            print("")
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
